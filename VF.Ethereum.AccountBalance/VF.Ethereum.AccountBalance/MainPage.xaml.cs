6f7f62ce10102a809dbf6b55c5c1e7dd1f1062d6
Vasiliy <vasiliy.fomichev@xcentium.com>
2018-09-26 19:17:00 -0700

    Add project files.

---
+++ VF.Ethereum.AccountBalance/VF.Ethereum.AccountBalance/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace VF.Ethereum.AccountBalance
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool _isRefreshing = false;
+        public bool IsRefreshing
+        {
+            get { return _isRefreshing; }
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    IsRefreshing = true;
+
+                    await RefreshData();
+
+                    IsRefreshing = false;
+                });
+            }
+        }
+
+        public async Task RefreshData()
+        {
+            return;
+        }
+    }
+}
